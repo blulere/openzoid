9e232ddaa33a104f3f4df39921dc2b03911dbb22
blulere <sharicm.305@outlook.com>
2025-04-23 22:07:55 +0100

    Update flip.js

--- effect/flip.js
+++ effect/flip.js
@@ -1,57 +1,85 @@
-this.defaultName = "Flip", this.shaderfile = "fx_repeat", this.shaderUrl = "/assets/shaders/fragment/" + this.shaderfile + ".glsl", this.vertShader = this.parentProject.assets.createFromPreset(PZ.asset.type.SHADER, "/assets/shaders/vertex/remap.glsl"), this.fragShader = this.parentProject.assets.createFromPreset(PZ.asset.type.SHADER, this.shaderUrl), this.propertyDefinitions = {
+this.defaultName = "Flip";
+this.shaderfile = "fx_repeat";
+this.shaderUrl = "/assets/shaders/fragment/" + this.shaderfile + ".glsl";
+this.vertShader = this.parentProject.assets.createFromPreset(
+    PZ.asset.type.SHADER,
+    "/assets/shaders/vertex/remap.glsl"
+);
+this.fragShader = this.parentProject.assets.createFromPreset(
+    PZ.asset.type.SHADER,
+    this.shaderUrl
+);
+
+this.propertyDefinitions = {
     enabled: {
         dynamic: true,
         name: "Enabled",
         type: PZ.property.type.OPTION,
         value: 1,
-        items: "off;on"
+        items: "off;on",
     },
     horizontal: {
         dynamic: true,
         name: "Horizontal flip",
         type: PZ.property.type.OPTION,
         value: 1,
-        items: "off;on"
+        items: "off;on",
     },
     vertical: {
         dynamic: true,
         name: "Vertical flip",
         type: PZ.property.type.OPTION,
         value: 0,
-        items: "off;on"
-    }
-}, this.properties.addAll(this.propertyDefinitions, this), this.load = async function(e) {
-    this.vertShader = new PZ.asset.shader(this.parentProject.assets.load(this.vertShader)), this.fragShader = new PZ.asset.shader(this.parentProject.assets.load(this.fragShader));
+        items: "off;on",
+    },
+};
+
+this.properties.addAll(this.propertyDefinitions, this);
+
+this.load = async function (e) {
+    this.vertShader = new PZ.asset.shader(
+        this.parentProject.assets.load(this.vertShader)
+    );
+    this.fragShader = new PZ.asset.shader(
+        this.parentProject.assets.load(this.fragShader)
+    );
     var t = new THREE.ShaderMaterial({
         uniforms: {
-            tDiffuse: {
-                type: "t",
-                value: null
-            },
-            uvScale: {
-                type: "v2",
-                value: new THREE.Vector2(1, 1)
-            },
-            uvTransform: {
-                type: "m3",
-                value: new THREE.Matrix3
-            }
+            tDiffuse: { type: "t", value: null },
+            uvScale: { type: "v2", value: new THREE.Vector2(1, 1) },
+            uvTransform: { type: "m3", value: new THREE.Matrix3() },
         },
         vertexShader: await this.vertShader.getShader(),
-        fragmentShader: await this.fragShader.getShader()
+        fragmentShader: await this.fragShader.getShader(),
     });
-    this.pass = new THREE.ShaderPass(t), this.pass.material.defines.REPEAT_MODE = 0, this.properties.load(e && e.properties)
-}, this.toJSON = function() {
-    return {
-        type: this.type,
-        properties: this.properties
+    this.pass = new THREE.ShaderPass(t);
+    this.pass.material.defines.REPEAT_MODE = 0;
+    this.properties.load(e && e.properties);
+};
+
+this.toJSON = function () {
+    return { type: this.type, properties: this.properties };
+};
+
+this.unload = function (e) {
+    this.parentProject.assets.unload(this.vertShader);
+    this.parentProject.assets.unload(this.fragShader);
+};
+
+this.update = function (e) {
+    if (!this.pass) {
+        return;
     }
-}, this.unload = function(e) {
-    this.parentProject.assets.unload(this.vertShader), this.parentProject.assets.unload(this.fragShader)
-}, this.update = function(e) {
-    if (!this.pass) return;
     this.pass.enabled = this.properties.enabled.get(e);
-    let t = this.properties.horizontal.get(e),
-        s = this.properties.vertical.get(e);
-    this.pass.uniforms.uvTransform.value.setUvTransform(0, 0, 1 - 2 * t, 1 - 2 * s, 0, .5, .5)
+    let horizontal = this.properties.horizontal.get(e);
+    let vertical = this.properties.vertical.get(e);
+    this.pass.uniforms.uvTransform.value.setUvTransform(
+        0,
+        0,
+        1 - 2 * horizontal,
+        1 - 2 * vertical,
+        0,
+        0.5,
+        0.5
+    );
 };
