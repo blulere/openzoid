847daf6a51c7b4cae04049448cf1ba177652c2d6
blulere <sharicm.305@outlook.com>
2025-04-23 22:47:38 +0100

    Update worker scripts

--- worker/av.js
+++ worker/av.js
@@ -1,101 +1,164 @@
 importScripts("av_init.js");
-var startTime, fs, fileEntry, fileWriter, now = Date.now,
-    lastErr = "";
+var startTime;
+var fs;
+var fileEntry;
+var fileWriter;
+var now = Date.now;
+var lastErr = "";
 
 function print(e) {
-    "object" == typeof e && (e = JSON.parse(JSON.stringify(e))), postMessage({
-        type: "stdout",
-        data: e
-    })
+    if (typeof e == "object") {
+        e = JSON.parse(JSON.stringify(e));
+    }
+    postMessage({ type: "stdout", data: e });
 }
 
 function printErr(e) {
-    "string" == typeof e && (lastErr = e), print(e)
+    if (typeof e == "string") {
+        lastErr = e;
+    }
+    print(e);
 }
 
 function quit(e, r) {
-    throw postMessage({
-        type: "error",
-        data: r.message + "\n" + lastErr
-    }), r
+    postMessage({ type: "error", data: r.message + "\n" + lastErr });
+    throw r;
 }
 
 function onReturn(e) {
-    for (var r = [], t = 0; t < e.length; t++) r.push(e[t].data);
-    postMessage({
-        type: "done",
-        data: e,
-        file: fileEntry ? fileEntry.file() : null,
-        time: now() - startTime
-    }, r)
+    var r = [];
+    for (var t = 0; t < e.length; t++) {
+        r.push(e[t].data);
+    }
+    postMessage(
+        {
+            type: "done",
+            data: e,
+            file: fileEntry ? fileEntry.file() : null,
+            time: now() - startTime,
+        },
+        r
+    );
 }
 
 function readCallback(e, r, t, a, f) {
-    var s = fileEntry.file(),
-        n = new Uint8Array(r.buffer, t, a),
-        _ = (new FileReaderSync).readAsArrayBuffer(s.slice(f, f + a));
-    return n.set(new Uint8Array(_)), a
+    var s = fileEntry.file();
+    var n = new Uint8Array(r.buffer, t, a);
+    var _ = new FileReaderSync().readAsArrayBuffer(s.slice(f, f + a));
+    n.set(new Uint8Array(_));
+    return a;
 }
 
 function writeCallback(e, r, t, a, f) {
-    var s = new Uint8Array(r.buffer, t, a),
-        n = new Blob([s]);
-    return fileWriter.write(n), a
+    var s = new Uint8Array(r.buffer, t, a);
+    var n = new Blob([s]);
+    fileWriter.write(n);
+    return a;
 }
 
 function seekCallback(e, r, t) {
-    return fileWriter.seek(r), r
+    fileWriter.seek(r);
+    return r;
 }
-var _heap, _sync, _callback, _buf_ptrs, _xfer_buffer, _xfer_ptrs, _xfer_ptrs_v, _xfer_ptrs_a, _remux, AV_NUM_DATA_POINTERS = 8,
-    HAS_THREADS = false;
+
+var _heap;
+var _sync;
+var _callback;
+var _buf_ptrs;
+var _xfer_buffer;
+var _xfer_ptrs;
+var _xfer_ptrs_v;
+var _xfer_ptrs_a;
+var _remux;
+var AV_NUM_DATA_POINTERS = 8;
+var HAS_THREADS = false;
 
 function getAudioSamples(e, r, t) {
-    if (_callback = t, _buf_ptrs = new Int32Array(_heap, e, AV_NUM_DATA_POINTERS), HAS_THREADS ? (_sync[0] = -1, _xfer_ptrs[0] = _buf_ptrs[0], _xfer_ptrs[1] = _buf_ptrs[1]) : _xfer_ptrs = _xfer_ptrs_a, postMessage({
+    _callback = t;
+    _buf_ptrs = new Int32Array(_heap, e, 8);
+    _xfer_ptrs = _xfer_ptrs_a;
+    postMessage(
+        {
             type: "audio",
             num: r,
             xfer_ptrs: [_xfer_ptrs[0], _xfer_ptrs[1]],
-            xfer_buffer: _xfer_buffer
-        }, [_xfer_buffer]), HAS_THREADS) return Atomics.wait(_sync, 0, -1), _sync[0]
+            xfer_buffer: _xfer_buffer,
+        },
+        [_xfer_buffer]
+    );
 }
 
 function getVideoFrame(e, r) {
-    if (_callback = r, _buf_ptrs = new Int32Array(_heap, e, AV_NUM_DATA_POINTERS), HAS_THREADS ? (_sync[0] = -1, _xfer_ptrs[0] = _buf_ptrs[0]) : _xfer_ptrs = _xfer_ptrs_v, postMessage({
+    _callback = r;
+    _buf_ptrs = new Int32Array(_heap, e, 8);
+    _xfer_ptrs = _xfer_ptrs_v;
+    postMessage(
+        {
             type: "video",
             xfer_ptrs: [_xfer_ptrs[0]],
-            xfer_buffer: _xfer_buffer
-        }, [_xfer_buffer]), HAS_THREADS) return Atomics.wait(_sync, 0, -1), _sync[0]
+            xfer_buffer: _xfer_buffer,
+        },
+        [_xfer_buffer]
+    );
 }
 
 function gotCallback(e, r) {
     for (var t = 0; t < _xfer_ptrs.length - 1; t++) {
         r = _xfer_ptrs[t + 1] - _xfer_ptrs[t];
-        var a = new Int8Array(_heap, _buf_ptrs[t], r),
-            f = new Int8Array(_xfer_buffer, _xfer_ptrs[t], r);
-        a.set(f)
+        var a = new Int8Array(_heap, _buf_ptrs[t], r);
+        var f = new Int8Array(_xfer_buffer, _xfer_ptrs[t], r);
+        a.set(f);
     }
-    _callback(e)
+    _callback(e);
 }
 
 function gotHeap(e) {
-    _heap = e, HAS_THREADS && postMessage({
-        type: "buffer",
-        buffer: _heap
-    })
+    _heap = e;
 }
 
 function gotRemux(e) {
-    _remux = e
+    _remux = e;
 }
-onmessage = function(e) {
-    if ("callback" === e.data.type) return _xfer_buffer = e.data.xfer_buffer, void gotCallback(e.data.ret, e.data.length);
-    if ("remux" !== e.data.type) {
-        if (e.data.syncBuffer && (_sync = new Int32Array(e.data.syncBuffer, 0, 1)), !HAS_THREADS && !e.data.options.decode) {
-            var r = e.data.options.frameWidth * e.data.options.frameHeight * 4,
-                t = Math.max(r, 7680);
-            _xfer_buffer = new ArrayBuffer(t), _xfer_ptrs_v = [0, r], _xfer_ptrs_a = [0, 3840, 7680]
+
+onmessage = function (e) {
+    if (e.data.type === "callback") {
+        _xfer_buffer = e.data.xfer_buffer;
+        gotCallback(e.data.ret, e.data.length);
+        return;
+    }
+    if (e.data.type === "remux") {
+        var s = _remux(e.data.options);
+        var n = [];
+        for (var _ = 0; _ < s.buffers.length; _++) {
+            n.push(s.buffers[_].data);
+        }
+        postMessage({ type: "remux", offset: s.offset, data: s.buffers }, n);
+    } else {
+        if (e.data.syncBuffer) {
+            _sync = new Int32Array(e.data.syncBuffer, 0, 1);
+        }
+        if (true && !e.data.options.decode) {
+            var r = e.data.options.frameWidth * e.data.options.frameHeight * 4;
+            var t = Math.max(r, 7680);
+            _xfer_buffer = new ArrayBuffer(t);
+            _xfer_ptrs_v = [0, r];
+            _xfer_ptrs_a = [0, 3840, 7680];
         }
         var a = 16777216;
-        1 === e.data.options.haveVideo && (a = 1024 * (a = 16 * Math.ceil((e.data.options.frameWidth * e.data.options.frameHeight / 17600 + 15) / 16)) * 1024);
+        if (e.data.options.haveVideo === 1) {
+            a =
+                1024 *
+                (a =
+                    16 *
+                    Math.ceil(
+                        ((e.data.options.frameWidth *
+                            e.data.options.frameHeight) /
+                            17600 +
+                            15) /
+                            16
+                    )) *
+                1024;
+        }
         var f = {
             pthreadMainPrefixURL: "/js/encoder/",
             options: e.data.options,
@@ -114,34 +177,29 @@ onmessage = function(e) {
             bufferRef: gotHeap,
             gotRemux: gotRemux,
             return: onReturn,
-            TOTAL_MEMORY: a
+            TOTAL_MEMORY: a,
         };
-        if (1 === e.data.options.haveVideo && !e.data.options.decode) try {
-            fs = webkitRequestFileSystemSync(PERSISTENT, 104857600);
+        if (e.data.options.haveVideo === 1 && !e.data.options.decode) {
             try {
-                fs.root.getFile("out", {
-                    create: false
-                }).remove()
-            } catch (e) {} finally {
-                fileEntry = fs.root.getFile("out", {
-                    create: true
-                }), fileWriter = fileEntry.createWriter()
+                fs = webkitRequestFileSystemSync(PERSISTENT, 104857600);
+                try {
+                    fs.root.getFile("out", { create: false }).remove();
+                } catch (e) {
+                } finally {
+                    fileEntry = fs.root.getFile("out", { create: true });
+                    fileWriter = fileEntry.createWriter();
+                }
+            } catch (e) {
+            } finally {
+                if (fs && fileEntry && fileWriter) {
+                    f.useDevFile = true;
+                }
             }
-        } catch (e) {} finally {
-            fs && fileEntry && fileWriter && (f.useDevFile = true)
         }
-        postMessage({
-            type: "start",
-            data: f.arguments
-        }), startTime = now(), ffmpeg_run(f)
-    } else {
-        for (var s = _remux(e.data.options), n = [], _ = 0; _ < s.buffers.length; _++) n.push(s.buffers[_].data);
-        postMessage({
-            type: "remux",
-            offset: s.offset,
-            data: s.buffers
-        }, n)
+        postMessage({ type: "start", data: f.arguments });
+        startTime = now();
+        ffmpeg_run(f);
     }
-}, postMessage({
-    type: "ready"
-});
+};
+
+postMessage({ type: "ready" });
--- worker/tar.js
+++ worker/tar.js
@@ -1,40 +1,52 @@
 importScripts("tar_init.js");
-var startTime, fs, fileEntry, fileWriter, now = Date.now;
+var startTime;
+var fs;
+var fileEntry;
+var fileWriter;
+var now = Date.now;
 
 function print(e) {
-    postMessage({
-        type: "stdout",
-        data: e
-    })
+    postMessage({ type: "stdout", data: e });
 }
 
 function onReturn(e) {
-    for (var t = [], r = 0; r < e.length; r++) t.push(e[r].data);
-    postMessage({
-        type: "done",
-        data: e,
-        file: fileEntry ? fileEntry.file() : null,
-        time: now() - startTime
-    }, t)
+    var t = [];
+    for (var r = 0; r < e.length; r++) {
+        t.push(e[r].data);
+    }
+    postMessage(
+        {
+            type: "done",
+            data: e,
+            file: fileEntry ? fileEntry.file() : null,
+            time: now() - startTime,
+        },
+        t
+    );
 }
 
 function readCallback(e, t, r, i, a) {
-    var n = fileEntry.file(),
-        l = new Uint8Array(t.buffer, r, i),
-        s = (new FileReaderSync).readAsArrayBuffer(n.slice(a, a + i));
-    return l.set(new Uint8Array(s)), i
+    var n = fileEntry.file();
+    var l = new Uint8Array(t.buffer, r, i);
+    var s = new FileReaderSync().readAsArrayBuffer(n.slice(a, a + i));
+    l.set(new Uint8Array(s));
+    return i;
 }
 
 function writeCallback(e, t, r, i, a) {
-    var n = new Uint8Array(t.buffer, r, i),
-        l = new Blob([n]);
-    return fileWriter.write(l), e.node.size = a + i, i
+    var n = new Uint8Array(t.buffer, r, i);
+    var l = new Blob([n]);
+    fileWriter.write(l);
+    e.node.size = a + i;
+    return i;
 }
 
 function seekCallback(e, t, r) {
-    return fileWriter.seek(t), t
+    fileWriter.seek(t);
+    return t;
 }
-onmessage = function(e) {
+
+onmessage = function (e) {
     var t = {
         files: e.data.files || [],
         wfiles: e.data.wfiles || [],
@@ -47,26 +59,31 @@ onmessage = function(e) {
         inputDirectory: e.data.inputDirectory,
         outputDirectory: e.data.outputDirectory,
         return: onReturn,
-        TOTAL_MEMORY: 16777216
+        TOTAL_MEMORY: 16777216,
     };
-    if (t.wfiles[0] && undefined === t.wfiles[0].name && (t.wfiles[0].name = "blob.pz"), true === e.data.useDevFile) try {
-        fs = webkitRequestFileSystemSync(PERSISTENT, 104857600);
-        try {
-            fs.root.getFile("out", {
-                create: false
-            }).remove()
-        } catch (e) {} finally {
-            fileEntry = fs.root.getFile("out", {
-                create: true
-            }), fileWriter = fileEntry.createWriter()
-        }
-    } catch (e) {} finally {
-        fs && fileEntry && fileWriter && (t.useDevFile = true)
+    if (t.wfiles[0] && undefined === t.wfiles[0].name) {
+        t.wfiles[0].name = "blob.pz";
     }
-    postMessage({
-        type: "start",
-        data: t.arguments
-    }), startTime = now(), tar_run(t)
-}, postMessage({
-    type: "ready"
-});
+    if (e.data.useDevFile === true) {
+        try {
+            fs = webkitRequestFileSystemSync(PERSISTENT, 104857600);
+            try {
+                fs.root.getFile("out", { create: false }).remove();
+            } catch (e) {
+            } finally {
+                fileEntry = fs.root.getFile("out", { create: true });
+                fileWriter = fileEntry.createWriter();
+            }
+        } catch (e) {
+        } finally {
+            if (fs && fileEntry && fileWriter) {
+                t.useDevFile = true;
+            }
+        }
+    }
+    postMessage({ type: "start", data: t.arguments });
+    startTime = now();
+    tar_run(t);
+};
+
+postMessage({ type: "ready" });
