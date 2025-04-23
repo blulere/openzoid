0824fc600d30a72b56224af7c64389d51aa65f80
blulere <sharicm.305@outlook.com>
2025-04-23 22:05:10 +0100

    Update exposure.js

--- effect/exposure.js
+++ effect/exposure.js
@@ -1,10 +1,22 @@
-this.defaultName = "Exposure", this.shaderfile = "fx_exposure", this.shaderUrl = "/assets/shaders/fragment/" + this.shaderfile + ".glsl", this.vertShader = this.parentProject.assets.createFromPreset(PZ.asset.type.SHADER, "/assets/shaders/vertex/common.glsl"), this.fragShader = this.parentProject.assets.createFromPreset(PZ.asset.type.SHADER, this.shaderUrl), this.propertyDefinitions = {
+this.defaultName = "Exposure";
+this.shaderfile = "fx_exposure";
+this.shaderUrl = "/assets/shaders/fragment/" + this.shaderfile + ".glsl";
+this.vertShader = this.parentProject.assets.createFromPreset(
+    PZ.asset.type.SHADER,
+    "/assets/shaders/vertex/common.glsl"
+);
+this.fragShader = this.parentProject.assets.createFromPreset(
+    PZ.asset.type.SHADER,
+    this.shaderUrl
+);
+
+this.propertyDefinitions = {
     enabled: {
         dynamic: true,
         name: "Enabled",
         type: PZ.property.type.OPTION,
         value: 1,
-        items: "off;on"
+        items: "off;on",
     },
     exposure: {
         dynamic: true,
@@ -13,7 +25,7 @@ this.defaultName = "Exposure", this.shaderfile = "fx_exposure", this.shaderUrl =
         value: 0,
         max: 1,
         min: -1,
-        step: .1
+        step: 0.1,
     },
     gamma: {
         dynamic: true,
@@ -22,40 +34,47 @@ this.defaultName = "Exposure", this.shaderfile = "fx_exposure", this.shaderUrl =
         value: 0,
         max: 1,
         min: -1,
-        step: .1
-    }
-}, this.properties.addAll(this.propertyDefinitions, this), this.load = async function(e) {
-    this.vertShader = new PZ.asset.shader(this.parentProject.assets.load(this.vertShader)), this.fragShader = new PZ.asset.shader(this.parentProject.assets.load(this.fragShader));
+        step: 0.1,
+    },
+};
+
+this.properties.addAll(this.propertyDefinitions, this);
+
+this.load = async function (e) {
+    this.vertShader = new PZ.asset.shader(
+        this.parentProject.assets.load(this.vertShader)
+    );
+    this.fragShader = new PZ.asset.shader(
+        this.parentProject.assets.load(this.fragShader)
+    );
     var t = new THREE.ShaderMaterial({
         uniforms: {
-            tDiffuse: {
-                type: "t",
-                value: null
-            },
-            uvScale: {
-                type: "v2",
-                value: new THREE.Vector2(1, 1)
-            },
-            exposure: {
-                type: "f",
-                value: 0
-            },
-            gamma: {
-                type: "f",
-                value: 0
-            }
+            tDiffuse: { type: "t", value: null },
+            uvScale: { type: "v2", value: new THREE.Vector2(1, 1) },
+            exposure: { type: "f", value: 0 },
+            gamma: { type: "f", value: 0 },
         },
         vertexShader: await this.vertShader.getShader(),
-        fragmentShader: await this.fragShader.getShader()
+        fragmentShader: await this.fragShader.getShader(),
     });
-    this.pass = new THREE.ShaderPass(t), this.pass.material.premultipliedAlpha = true, this.properties.load(e && e.properties)
-}, this.toJSON = function() {
-    return {
-        type: this.type,
-        properties: this.properties
+    this.pass = new THREE.ShaderPass(t);
+    this.pass.material.premultipliedAlpha = true;
+    this.properties.load(e && e.properties);
+};
+
+this.toJSON = function () {
+    return { type: this.type, properties: this.properties };
+};
+
+this.unload = function (e) {
+    this.parentProject.assets.unload(this.vertShader);
+    this.parentProject.assets.unload(this.fragShader);
+};
+
+this.update = function (e) {
+    if (this.pass) {
+        this.pass.uniforms.exposure.value = this.properties.exposure.get(e);
+        this.pass.uniforms.gamma.value = this.properties.gamma.get(e);
+        this.pass.enabled = this.properties.enabled.get(e) === 1;
     }
-}, this.unload = function(e) {
-    this.parentProject.assets.unload(this.vertShader), this.parentProject.assets.unload(this.fragShader)
-}, this.update = function(e) {
-    this.pass && (this.pass.uniforms.exposure.value = this.properties.exposure.get(e), this.pass.uniforms.gamma.value = this.properties.gamma.get(e), this.pass.enabled = 1 === this.properties.enabled.get(e))
 };
